fccfa09595542047354210a845a0612756c19195
N.Karthik Akshaj <akshajnk@gmail.com>
2025-02-27 17:56:01 +0530

    Added delete and copy feature in nodes

--- app/workflow/_components/nodes/NodeHeader.tsx
+++ app/workflow/_components/nodes/NodeHeader.tsx
@@ -1,33 +1,62 @@
-"use client"
+"use client";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { TaskType } from "@/types/task";
-import { CoinsIcon, GripVerticalIcon } from "lucide-react";
+import { useReactFlow } from "@xyflow/react";
+import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from "lucide-react";
 
-function NodeHeader({ taskType }: { taskType: TaskType }) {
-    const task = TaskRegistry[taskType];
-  
-    return (
-      <div className="flex items-center gap-2 p-2">
-        <task.icon size={16} />
-        <div className="flex justify-between items-center w-full">
-          <p className="font-bold uppercase">{task.label}</p>
-          <div className="flex gap-1 items-center">
-            {task.isEntryPoint && <Badge>Entry point</Badge>}
-            <Badge className="flex gap-2 items-center text-xs">
-              <CoinsIcon size={16} />
-              TODO
-            </Badge>
-            <Button variant={"ghost"} size={"icon"} className=" drag-handle cursor-grab">
-              <GripVerticalIcon size={20} />
-            </Button>
-          </div>
+function NodeHeader({
+  taskType,
+  nodeId,
+}: {
+  taskType: TaskType;
+  nodeId: string;
+}) {
+  const task = TaskRegistry[taskType];
+  const { deleteElements } = useReactFlow();
+
+  return (
+    <div className="flex items-center gap-2 p-2">
+      <task.icon size={16} />
+      <div className="flex justify-between items-center w-full">
+        <p className="font-bold uppercase">{task.label}</p>
+        <div className="flex gap-1 items-center">
+          {task.isEntryPoint && <Badge>Entry point</Badge>}
+          <Badge className="flex gap-2 items-center text-xs">
+            <CoinsIcon size={16} />
+            TODO
+          </Badge>
+          {!task.isEntryPoint && (
+            <>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                onClick={() => {
+                  deleteElements({
+                    nodes: [{ id: nodeId }],
+                  });
+                }}
+              >
+                <TrashIcon size={12} />
+              </Button>
+              <Button variant={"ghost"} size={"icon"}>
+                <CopyIcon size={12} />
+              </Button>
+            </>
+          )}
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className=" drag-handle cursor-grab"
+          >
+            <GripVerticalIcon size={20} />
+          </Button>
         </div>
       </div>
-    );
-  }
-  
-  export default NodeHeader;
-  
+    </div>
+  );
+}
+
+export default NodeHeader;
