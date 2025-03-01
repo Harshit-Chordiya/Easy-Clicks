3b9df308b6def69726cb6ff36157a62541d8ca4d
Harshit-Chordiya <harshitchordiya03@gmail.com>
2025-03-01 14:45:38 +0530

    execution workflow

--- components/ReactCountUpWrapper.tsx
+++ components/ReactCountUpWrapper.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useMounted } from "@/hooks/";
+import { useMounted } from "@/hooks/use-mounted";
 import React from "react";
 import CountUp from "react-countup";
 
-export default function ReactCountUpWrapper({ value }: { value: number }) {
-    const mounted = useMounted();
-    if (!mounted) return "-";
 
-    return <CountUp duration={0.5} preserveValue end={value} decimals={0} />;
+export default function ReactCountUpWrapper({ value }: { value: number }) {
+  const mounted = useMounted();
+  if (!mounted) return "-";
+
+  return <CountUp duration={0.5} preserveValue end={value} decimals={0} />;
 }
