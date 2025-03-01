e81804035ec7324ce0fe809dfd9cb0a798571eec
N.Karthik Akshaj <akshajnk@gmail.com>
2025-03-01 19:23:39 +0530

    Feat: Fill input automation

--- app/workflow/_components/TaskMenu.tsx
+++ app/workflow/_components/TaskMenu.tsx
@@ -6,26 +6,64 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { TaskType } from "@/types/task";
-import React from "react";
+import { CoinsIcon } from "lucide-react";
 
 export default function TaskMenu() {
   return (
-    <aside className=" w-[340px] min-w-[340px] max-w-[340px] border-r-2 border-separate h-full p-2 px-4 overflow-auto">
+    <aside className="h-full w-[350px] min-w-[350px] max-w-[350px] border-separate overflow-auto border-r-2 p-2 px-4">
       <Accordion
         type="multiple"
         className="w-full"
-        defaultValue={["extraction"]}
+        defaultValue={[
+          "interactions",
+          "extraction",
+          "storage",
+          "timing",
+          "results",
+        ]}
       >
+        <AccordionItem value="interactions">
+          <AccordionTrigger>User Interactions</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.NAVIGATE_URL} />
+            <TaskMenuBtn taskType={TaskType.FILL_INPUT} />
+            <TaskMenuBtn taskType={TaskType.CLICK_ELEMENT} />
+            <TaskMenuBtn taskType={TaskType.SCROLL_TO_ELEMENT} />
+          </AccordionContent>
+        </AccordionItem>
+
         <AccordionItem value="extraction">
-          <AccordionTrigger className="font-bold">
-            Data extraction
-          </AccordionTrigger>
+          <AccordionTrigger>Data Extraction</AccordionTrigger>
           <AccordionContent className="flex flex-col gap-1">
             <TaskMenuBtn taskType={TaskType.PAGE_TO_HTML} />
             <TaskMenuBtn taskType={TaskType.EXTRACT_TEXT_FROM_ELEMENT} />
+            <TaskMenuBtn taskType={TaskType.EXTRACT_DATA_WITH_AI} />
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="storage">
+          <AccordionTrigger>Storage</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.READ_PROPERTY_FROM_JSON} />
+            <TaskMenuBtn taskType={TaskType.ADD_PROPERTY_TO_JSON} />
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="timing">
+          <AccordionTrigger>Timing Controls</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.WAIT_FOR_ELEMENT} />
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="results">
+          <AccordionTrigger>Result delivery</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.DELIVER_VIA_WEBHOOK} />
           </AccordionContent>
         </AccordionItem>
       </Accordion>
@@ -35,14 +73,19 @@ export default function TaskMenu() {
 
 function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
   const task = TaskRegistry[taskType];
-  const onDragStart = (event: React.DragEvent, type: TaskType) => {
-    event.dataTransfer.setData("application/reactflow", type);
+
+  const onDragStart = (
+    event: React.DragEvent<HTMLButtonElement>,
+    taskType: TaskType,
+  ) => {
+    event.dataTransfer.setData("application/reactflow", taskType);
     event.dataTransfer.effectAllowed = "move";
   };
+
   return (
     <Button
-      variant={"secondary"}
-      className="flex justify-between items-center gap-2 border w-full"
+      variant="secondary"
+      className="flex w-full items-center justify-between gap-2 border"
       draggable
       onDragStart={(event) => onDragStart(event, taskType)}
     >
@@ -50,6 +93,10 @@ function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
         <task.icon size={20} />
         {task.label}
       </div>
+      <Badge className="flex items-center gap-2" variant="outline">
+        <CoinsIcon size={16} />
+        {task.credits}
+      </Badge>
     </Button>
   );
 }
--- lib/workflow/executor/registry.ts
+++ lib/workflow/executor/registry.ts
@@ -1,22 +1,38 @@
 import { TaskType } from "@/types/task";
 import { LaunchBrowserExecutor } from "./LaunchBrowserExecutor";
+import { PageToHtmlExecutor } from "./PageToHtmlExecutor";
 import { WorkflowTask } from "@/types/workflow";
 import { ExecutionEnvironment } from "@/types/executor";
-import { PageToHtmlExecutor } from "./PageToHtmlExecutor";
 import { ExtractTextFromElementExecutor } from "./ExtractTextFromElementExecutor";
+import { FillInputExecutor } from "./FillInputExecutor";
+import { ClickElementExecutor } from "./click-element-executor";
+import { WaitForElementExecutor } from "./wait-for-element-executor";
+import { DeliverViaWebhookExecutor } from "./deliver-via-webhook-executor";
+import { ExtractDataWithAIExecutor } from "./extract-data-with-ai-executor";
+import { ReadPropertyFromJsonExecutor } from "./read-property-from-json-executor";
+import { AddPropertyToJsonExecutor } from "./add-property-to-json-executor";
+import { NavigateUrlExecutor } from "./navigate-url-executor";
+import { ScrollToElementExecutor } from "./scroll-to-element-executor";
 
-type ExecutorFn<T extends WorkflowTask> = (
+type ExecutorFunction<T extends WorkflowTask> = (
   environment: ExecutionEnvironment<T>,
 ) => Promise<boolean>;
 
 type RegistryType = {
-  [K in TaskType]: ExecutorFn<WorkflowTask & { type: K }>;
+  [K in TaskType]: ExecutorFunction<WorkflowTask & { type: K }>;
 };
 
-export const ExecutorRegistry = {
+export const ExecutorRegistry: RegistryType = {
   LAUNCH_BROWSER: LaunchBrowserExecutor,
   PAGE_TO_HTML: PageToHtmlExecutor,
   EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementExecutor,
-
-
+  FILL_INPUT: FillInputExecutor,
+  CLICK_ELEMENT: ClickElementExecutor,
+  WAIT_FOR_ELEMENT: WaitForElementExecutor,
+  DELIVER_VIA_WEBHOOK: DeliverViaWebhookExecutor,
+  EXTRACT_DATA_WITH_AI: ExtractDataWithAIExecutor,
+  READ_PROPERTY_FROM_JSON: ReadPropertyFromJsonExecutor,
+  ADD_PROPERTY_TO_JSON: AddPropertyToJsonExecutor,
+  NAVIGATE_URL: NavigateUrlExecutor,
+  SCROLL_TO_ELEMENT: ScrollToElementExecutor,
 };
