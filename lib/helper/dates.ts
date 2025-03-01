0a842da056dc3ef6c711ac36aa3c187bb19058e8
N.Karthik Akshaj <akshajnk@gmail.com>
2025-03-01 23:03:59 +0530

    Add: part of home page design done

--- lib/helper/dates.ts
+++ lib/helper/dates.ts
@@ -1,21 +1,42 @@
-import { intervalToDuration } from "date-fns";
+import { Period } from "@/types/analytics";
+import { clsx, type ClassValue } from "clsx";
+import { endOfMonth, intervalToDuration, startOfMonth } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
-export function DatesToDurationString(
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function datesToDurationString(
   end: Date | null | undefined,
-  start: Date | null | undefined
+  start: Date | null | undefined,
 ) {
-  if (!start || !end) return null;
+  if (!end || !start) return null;
 
   const timeElapsed = end.getTime() - start.getTime();
   if (timeElapsed < 1000) {
-    
     return `${timeElapsed}ms`;
   }
 
   const duration = intervalToDuration({
-    start:0,
+    start: 0,
     end: timeElapsed,
   });
 
-  return ` ${duration.minutes || 0}m ${duration.seconds || 0}s`;
+  return `${duration.minutes || 0}m ${duration.seconds || 0}s`;
+}
+
+export function getAppUrl(path: string) {
+  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+  return `${appUrl}${path}`;
+}
+
+export function periodToDateRange(period: Period) {
+  const startDate = startOfMonth(new Date(period.year, period.month, 1));
+  const endDate = endOfMonth(new Date(period.year, period.month, 1));
+  return { startDate, endDate };
 }
